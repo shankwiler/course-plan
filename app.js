4d8e3532eff2c15940f3217f472f969ee2013d85
shankwiler <kyleshankwiler@gmail.com>
2016-02-07 23:39:11 -0800

    remove users endpoint

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var parser = require('./routes/parser');
 
 var app = express();

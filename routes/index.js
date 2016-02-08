04b229d03a14383da797dd62cad2a81557d9bf1c
shankwiler <kyleshankwiler@gmail.com>
2016-02-07 23:32:09 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,303 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var http = require('http');
+var plans = require('../data/plans.json');
+var ccs = require('../data/ccs.json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'course-plan' });
+});
+
+router.get(/^\/plan.*/, function(req, res) {
+  var urlSplit, cc, year, uniMajors, rank, valid;
+  urlSplit = req.url.split('/').slice(2);
+  // get rid of trailing whitespace if ending / is added
+  if (urlSplit[urlSplit.length - 1] === '')
+    urlSplit = urlSplit.slice(0, -1);
+  if (urlSplit.length < 3) {
+    res.status(400).send('format: /plan/communitycollege/year/uni,major/uni,major/');
+    return;
+  }
+  cc = urlSplit[0];
+  year = urlSplit[1];
+  if (!(cc in ccs) || !(cc in plans)) {
+    res.status(404).send(cc + ' not found');
+    return;
+  }
+  if (!(year in plans[cc])) {
+    res.status(404).send(year + ' not found for ' + cc);
+    return;
+  }
+  
+  // generate the list of university/major combinations
+  uniMajors = [];
+  var valid = true;
+  urlSplit.slice(2).forEach((item) => {
+    if (valid) {
+      var separated = item.split(',');
+      var currUni = separated[0];
+      var currMajor = separated[1];
+      if (separated.length > 1 &&
+        currUni in plans[cc][year] && currMajor in plans[cc][year][currUni])
+        uniMajors.push({'uni': currUni, 'major': currMajor});
+      else {
+        res.status(404).end(item + ' not found');
+        valid = false;
+      }
+    }
+  });
+  if (valid) {
+    // generate json w/ courses, universities they're for, and their unit counts
+    var data, newData;
+    data = courseLists(cc, year, uniMajors)[0];
+    // newData stores the courses, their units, and universities they're for
+    newData = {'courses':{}};
+    Object.keys(data['courses']).forEach((crs) => {
+      var unis, crsInfo;
+      unis = data['courses'][crs];
+      crsInfo = {
+        'unis': unis,
+        'units': ccs[cc][year][crs]
+      };
+      newData['courses'][crs] = crsInfo;
+    });
+    newData['units'] = data['units'];
+    
+    res.json(newData);
+  }
+});
+
+router.get(/^\/unis.*/, function(req, res) {
+  var split, cc, year;
+  split = req.url.split('/');
+  // get rid of trailing whitespace if ending / is added
+  if (split[split.length - 1] === '')
+    split = split.slice(0, -1);
+  if (split.length !== 4) {
+    res.status(400).send('format: /unis/communitycollege/year');
+    return;
+  }
+  cc = split[2];
+  year = split[3];
+  if (!(cc in plans)) {
+    res.status(404).send(cc + ' not found');
+    return;
+  }
+  if (!(year in plans[cc])) {
+    res.status(404).send(year + ' not found for ' + cc);
+    return;
+  }
+  var ret = {};
+  Object.keys(plans[cc][year]).forEach(function(uni) {
+    if (uni !== 'name')
+      ret[uni] = plans[cc][year][uni]['name'];
+  });
+  res.json(ret);
+});
+
+router.get(/^\/majors.*/, function(req, res) {
+  var urlSplit, college, uni, year;
+  urlSplit = req.url.split('/').slice(2);
+  // get rid of trailing whitespace if ending / is added
+  if (urlSplit[urlSplit.length - 1] === '')
+    urlSplit = urlSplit.slice(0, -1);
+  if (urlSplit.length !== 3) {
+    res.status(400).end('format: /majors/communitycollege/year/university/');
+    return;
+  }
+  college = urlSplit[0];
+  year = urlSplit[1];
+  uni = urlSplit[2];
+  if (!(college in plans)) {
+    res.status(404).end(college + ' not found');
+    return;
+  }
+  if (!(year in plans[college])) {
+    res.status(404).end(year + ' not found as a year for ' + college);
+    return;
+  }
+  if (!(uni in plans[college][year])) {
+    res.status(404).end(uni + ' not found as a university for ' + college
+      + ' in ' + year);
+    return;
+  }
+  var ret = {};
+  Object.keys(plans[college][year][uni]).forEach(function(major) {
+    if (major !== 'name')
+      ret[major] = plans[college][year][uni][major]['name'];
+  });
+  res.json(ret);
+});
+
+router.get(/^\/years.*/, (req, res) => {
+  var split, cc, yrs, nameInd;
+  split = req.url.split('/');
+  cc = split[2];
+  if(split[split.length -1] === '')
+    split = split.slice(0, -1);
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end('format: /years/cc/');
+    return;
+  }
+  if (!plans[cc]) {
+    res.status(404).end('cc not found');
+    return;
+  }
+  yrs = Object.keys(plans[cc]);
+  nameInd = yrs.indexOf('name');
+  yrs = yrs.slice(0, nameInd).concat(yrs.slice(nameInd + 1));
+  res.json(yrs);
+});
+
+router.get('/ccs', (req, res) => {
+  var ret = {};
+  Object.keys(plans).forEach((cc) => {
+    ret[cc] = plans[cc]['name'];
+  });
+  res.json(ret);
+});     
+
+// TODO look back over airbnb style guide. Find things to fix such as dot
+// vs bracket notation etc.
+// TODO required courses may be able to be shoved into the compilation of 
+// data for course options, rather than doing it separately (w/ reqs).
+// may be cleaner.
+
+function courseLists(cc, year, uniMajors) {
+  // The meat of this site. Finds the optimal course plan
+  var reqs = {};
+  
+  // add the required courses for each university
+  uniMajors.forEach((uniMajor) => {
+    var plan = plans[cc][year][uniMajor.uni][uniMajor.major];
+    plan['required'].forEach((crs) => {
+      if (crs in reqs)
+        reqs[crs].push(uniMajor.uni);
+      else
+        reqs[crs] = [uniMajor.uni];
+    });
+  });
+  
+  // compile the data of course options
+  var data = [];
+  uniMajors.forEach((uniMajor) => {
+    var plan = plans[cc][year][uniMajor.uni][uniMajor.major];
+    plan['choices'].forEach((choice) => {
+      data.push({'uni':uniMajor.uni, 'courses': choice});
+    });
+    //console.log('plan', plan);
+    plan['choosenum'].forEach((group) => {
+      data.push({
+        'uni': uniMajor.uni, 
+        'courses': chooseK(group['choices'], group['num'])
+      });
+    });
+  });
+  
+  // find the most efficient combination
+  var combos = findCombos(data);
+  var courseLists = []; // an array of course combinations
+  combos.forEach((combo) => {
+    var courseList = {};
+    Object.keys(reqs).forEach((key) => {
+      courseList[key] = reqs[key].slice();
+    });
+    combo.forEach((grp) => {
+      grp['courses'].forEach((crs) => {
+        if (crs in courseList) {
+          if (courseList[crs].indexOf(grp['uni']) == -1) {
+            courseList[crs].push(grp['uni']);
+          }
+        }
+        else
+          courseList[crs] = [grp['uni']];
+      });
+    });
+    courseLists.push(courseList);
+  });
+  
+  // change the course lists to include their unit count
+  var courseLists = courseLists.map((crsList) => {
+    var unitCnt = 0;
+    Object.keys(crsList).forEach((crs) => {
+      unitCnt += ccs[cc][year][crs];
+    });
+    return {'courses': crsList, 'units': unitCnt}
+  });
+  
+  // sort the list so lowest count is first
+  courseLists.sort((lstA, lstB) => {
+    if (lstA['units'] > lstB['units'])
+      return 1;
+    if (lstA['units'] < lstB['units'])
+      return -1;
+    return 0;
+  });
+  
+  // every possible course path is returned, with the optimal one
+  // being in the 0 index
+  return courseLists;
+}
+
+function findCombos(data) {
+  // Takes an array of objects as follows:
+  // [{
+  // 'uni':'uni name',
+  // 'courses': [
+  //    [ // option 1
+  //      course 1, course 2
+  //    ],
+  //    [ //option 2
+  //      course 3, course 4
+  //    ]
+  // }]
+  // the different options are compiled into a list of possible options
+    
+  // recursive helper function
+  function makeCombos(data, combos, ind, chain) {
+    // combos is an array of possible combinations, ind is the current
+    // index of courses being looked at, chain is the previously chosen
+    // course group for this possibility
+    if (ind == data.length)
+      combos.push(chain);
+    else {
+      data[ind]['courses'].forEach(function(grp) {
+        newChain = chain.slice();
+        newChain.push({
+          'uni': data[ind]['uni'],
+          'courses': grp
+        });
+        makeCombos(data, combos, ind + 1, newChain);
+      });
+    }
+  }
+  combos = [];
+  makeCombos(data, combos, 0, []);
+  return combos;
+}
+
+function chooseK(data, k) {
+  // this function generates course group options, for the case where
+  // there is a choosenum set. For example: choose 2 from ['a','b','c']
+  // will return [['a','b'],['a','c'],['b','c']]
+  
+  // helper function acts similarly to the makeComos function in findCombos()
+  function makeCombos(data, combos, k, chain, ind) {
+    if (chain.length == k)
+      combos.push(chain);
+    else {
+      for (var i = ind + 1; i < data.length; i++) {
+        var newChain = chain.slice();
+        newChain.push(data[i]);
+        makeCombos(data, combos, k, newChain, i);
+      } 
+    } 
+  }
+  combos = [];
+  makeCombos(data, combos, k, [], -1);
+  return combos;
+}
+
+module.exports = router;
---
+++ routes/parser.js
@@ -0,0 +1,502 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var fs = require('fs');
+var plans = require('../data/plans.json');
+var ccs = require('../data/ccs.json');
+
+router.get('/', function(req, res, next) {
+  res.render('parser', {title: 'parse' });
+});
+
+router.get('/ccs', (req, res) => {
+  // TODO use loadOptions function
+  
+  http.get('http://www.assist.org/web-assist/welcome.html', (aRes) => {
+    var html;
+    html = '';
+    
+    aRes.setEncoding('utf8');
+    aRes.on('data', (chunk) => {
+      html += chunk;
+    });
+    aRes.on('end', () => {
+      var list;
+      list = html.split('</option>').slice(1, -1).map((line) => {
+        return dataFromLine(line);
+      });
+      res.json({opts:list});
+    });
+  }).on('error', (e) => {
+    console.log(e);
+  });
+});
+
+router.get(/^\/years.*/, (req, res) => {
+  var split, cc;
+  split = req.url.split('/');
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end();
+    return;
+  }
+  cc = req.url.split('/')[2];
+  pullOpts('http://www.assist.org/web-assist/' + cc, 'helptextAY\')">', (data) => {
+    if (data.status !== 200)
+      res.status(data.status).end();
+    else
+      res.json({opts:data.json});
+  });
+});
+
+router.get(/^\/unis.*/, (req, res) => {
+  var split, assistUrl;
+  split = req.url.split('/');
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end();
+    return;
+  }
+  assistUrl = split[2];
+  console.log(assistUrl);
+  pullOpts('http://www.assist.org/web-assist/' + assistUrl, 'campus<', (data) => {
+    if (data.status !== 200)
+      res.status(data.status).end();
+    else
+      res.json({opts:data.json});
+  });
+});
+
+router.get(/^\/majors.*/, (req, res) => {
+  var split, assistUrl;
+  split = req.url.split('/');
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end();
+    return;
+  }
+  assistUrl = split[2];
+  pullOpts('http://www.assist.org/web-assist/' + assistUrl, 'Select a major', (data) => {
+    if (data.status !== 200)
+      res.status(data.status).end();
+    else {
+      var ret;
+      ret = {};
+      ret.opts = data.json;
+      ret.submitUrl = parseMajorForm(data.html);
+      res.json(ret);
+    }
+  });
+});
+
+
+router.get(/^\/plan.*/, (req, res) => {
+  var split, assistUrl;
+  split = req.url.split('/');
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end();
+    return;
+  }
+  assistUrl = split[2];
+  getPlan(assistUrl, (planUrl) => {
+    res.end(planUrl);
+  });
+});
+
+router.get(/^\/data.*/, (req, res) => {
+  var split, cc, year, uni, major;
+  split = req.url.split('/');
+  if (split.length !== 6 || split[5] === '') {
+    res.status(400).end();
+    return;
+  }
+  cc = split[2];
+  year = split[3];
+  uni = split[4];
+  major = split[5];
+  if (plans[cc] && plans[cc][year] && plans[cc][year][uni] && plans[cc][year][uni][major]) {
+    var majorData = plans[cc][year][uni][major];
+    var coursePlan = {
+      'required': majorData['required'],
+      'choices': majorData['choices'],
+      'choosenum': majorData['choosenum'],
+      'missing': majorData['missing'],
+    };
+    res.json({
+      'courses': coursePlan,
+      'units': findCourseUnitsForPlan(cc, year, coursePlan)
+    });
+  }
+  else
+    res.status(404).end('not found');
+});
+
+router.post(/^\/data.*/, (req, res) => {
+  var split, cc, year, uni, major;
+  split = req.url.split('/');
+  if (split.length !== 6 || split[5] === '') {
+    res.status(400).end();
+    return;
+  }
+  cc = split[2];
+  year = split[3];
+  uni = split[4];
+  major = split[5];
+  
+  if (!plans[cc]) {
+    plans[cc] = {};
+    plans[cc]['name'] = req.body.college_name;
+  }
+  console.log('hi.125');
+  if (!plans[cc][year])
+    plans[cc][year] = {};
+  console.log('hi.25');
+  if (!plans[cc][year][uni]) {
+    plans[cc][year][uni] = {};
+    plans[cc][year][uni]['name'] = req.body.uni_name;
+  }
+  console.log('hi.5');
+  if (!plans[cc][year][uni][major])
+    plans[cc][year][uni][major] = {};
+  
+  console.log('hi1');
+  
+  plans[cc][year][uni][major] = JSON.parse(req.body.courses);
+  plans[cc][year][uni][major]['name'] = req.body.major_name;
+  
+  
+  /*console.log(JSON.stringify(req.body.courses, null, '  ').replace(/\\n/g, '\n').replace(/\\\"/g, '"'));*/
+  
+  fs.writeFile(__dirname + '/../data/plans.json', JSON.stringify(plans, null, '  '), (err) => {
+    if (err)
+      throw err;
+    console.log('saved courses');
+  });
+  
+  if(!ccs[cc])
+    ccs[cc] = {};
+  if(!ccs[cc][year])
+    ccs[cc][year] = {};
+  
+  updateObject(ccs[cc][year], JSON.parse(req.body.units));
+  
+  fs.writeFile(__dirname + '/../data/ccs.json', JSON.stringify(ccs, null, '  '), (err) => {
+    if (err)
+      throw err;
+    console.log('saved units');
+  });
+  
+  res.send('success');
+});
+
+router.get(/^\/guess.*/, (req, res) => {
+  var split, assistUrl;
+  split = req.url.split('/');
+  if (split.length !== 3 || split[2] === '') {
+    res.status(400).end();
+    return;
+  }
+  assistUrl = split[2];
+  getPlan(assistUrl, (planUrl) => {
+    http.get(planUrl, (aRes) => {
+      if (aRes.statusCode !== 200) {
+        res.status(aRes.statusCode).end();
+        return;
+      }
+      var html;
+      html = '';
+      aRes.setEncoding('utf8');
+      aRes.on('data', (chunk) => {
+        html += chunk;
+      });
+      aRes.on('end', () => {
+        res.json({
+          'plan': parsePlan(html),
+          'units': getUnits(html)
+        });
+      });
+    });
+  });
+});
+
+function dataFromLine(line) {
+  return {
+    link: line.substring(line.search('value="') + 7, line.search('">')),
+    name: line.substring(line.search('">') + 2)
+  }
+}
+
+function getPlan(url, cb) {
+  var fullUrl = 'http://www.assist.org/web-assist/' + url;
+  http.get(fullUrl, (res) => {
+    if (res.statusCode !== 200) {
+      cb('invalid url');
+      return;
+    }
+    var html;
+    html = '';
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      html += chunk;
+    });
+    res.on('end', () => {
+      var planUrl;
+      console.log(planUrl);
+      planUrl = html.split('<iframe')[1].split('src="')[1].split('"')[0];
+      //getPlanText(planUrl, cb);
+      cb(planUrl);
+    });
+  }).on('error', (e) => {
+    console.log(e);
+  });
+}
+
+function pullOpts(url, sep, callback) {
+  // returns json with status, json (list of options), html
+  // TODO refactor code so one function returns html, another parses,
+  // would be much cleaner
+  http.get(url, (res) => {
+    if (res.statusCode !== 200) {
+      callback({
+        status: res.statusCode
+      });
+    }
+    else {
+      var html;
+      html = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        html += chunk;
+      });
+      
+      res.on('end', () => {
+        var list, block, lines, valSubstr, endValLoc, link, uni;
+        if (html.indexOf(sep) === -1) {
+          callback({status: 404});
+          return;
+        }
+        block = html.split(sep)[1].split('</select>')[0];
+        lines = block.split('</option>').slice(0, -1);
+        list = lines.map((line) => {
+          valSubstr = line.substring(line.search('value="') + 7);
+          endValLoc = valSubstr.search('"');
+          link = valSubstr.substring(0, endValLoc).replace('&amp;', '&');
+          uni = valSubstr.substring(valSubstr.search('>') + 1);
+          return {
+            link: link,
+            name: uni
+          }
+        });
+        callback({
+          status: 200,
+          json: list,
+          html: html
+        });
+      });                   
+    }
+  }).on('error', (e) => {
+    console.log(e);
+  });
+}
+
+function parseMajorForm(html) {
+  var page, qstr;
+  html = html.split('<form name="major"')[1].split('</select>')[0];
+  page = html.substring(html.indexOf('action="') + 8);
+  page = page.substring(0, page.indexOf('">'));
+  
+  // for the inputs before the major select box, get their name and value
+  qstr = '';
+  html.split('<select')[0].split('\n').forEach(function (line) {
+    if (line.indexOf('<input') !== -1) {
+      var name, val;
+      name = line.split('name="')[1].split('"')[0];
+      val = line.split('value="')[1].split('"')[0];
+      qstr += name + '=' + val + '&';
+    }
+  })
+  qstr = qstr.substring(0, qstr.length - 1)
+  
+  return page + '?' + qstr;
+}
+
+function parsePlan(data) {
+  // TODO put code into functions to make easier to read.
+  
+  var sep = /--------------------------------------------------------------------------------|AND/;
+  var reOr = />\s*OR\s*<.*\|/;
+  
+  var blocks = data.split(sep);
+  
+  var courses = {
+    'required': [],
+    'choices': [],
+    'choosenum': [],
+    'missing': []
+  };
+  blocks.forEach((block) => {
+    if (reOr.exec(block)) {
+      var grps, opts, allMiss;
+      grps = block.split(reOr);
+      opts = [];
+      grps.forEach((grp) => {
+        opts.push(getCourses(grp));
+      });
+      
+      allMiss = true;
+      opts.forEach((opt) => {
+        opt.forEach((crs) => {
+          if (crs.split(' ')[0] !== 'missing')
+            allMiss = false;
+        });
+      });
+      
+      if (allMiss) {
+        var str = '';
+        opts.forEach((opt, i) => {
+          if (i !== 0)
+            str += ' or ';
+          str += opt.map((crs) => {
+            return crs.substr(crs.search(' ') + 1);
+          }).join(' and ');
+        });
+        courses.missing.push(str);
+      }
+      else {
+        var newOpts;
+        newOpts = [];
+        opts.forEach((opt) => {
+          var valid;
+          valid = true;
+          opt.forEach((crs) => {
+            if (crs.includes('missing'))
+              valid = false;
+          });
+          if (valid)
+            newOpts.push(opt);
+        });
+        
+        if (newOpts.length === 1) {
+          newOpts[0].forEach((crs) => {
+            courses.required.push(crs);
+          });
+        }
+        else
+          courses.choices.push(opts);
+           
+      }
+    }
+    else {
+      getCourses(block).forEach((crs) => {
+        if (crs.split(' ')[0] === 'missing') {
+          var crsName;
+          crsName = crs.split(' ').slice(1).join(' ');
+          if (courses.missing.indexOf(crsName) === -1)
+            courses.missing.push(crsName);
+        }
+        else if (courses.required.indexOf(crs) === -1)
+          courses.required.push(crs);
+      });
+    }
+  });
+  
+  return courses;
+}
+
+function getCourses(str) {
+  var courses, prevAnd, prevMissing;
+  prevAnd = null;
+  prevMissing = false;
+  courses = [];
+  str.split('\n').forEach((line) => {
+    var ccCourse = /\([0-9]+(\.[0-9]+)?\)$/.exec(line); // (0-9) found at end of line
+    var uniCourse = /\([0-9]+(\.[0-9]+)?\)\|/.exec(line); // (0-9)| found in line
+    
+    // if there's a uni course and no cc course and either there was no &
+    // sign on the past uni course, or there was and that course was missing
+    if (uniCourse && !ccCourse && 
+      (!prevAnd || (prevAnd && prevMissing))) {
+      courses.push('missing ' + line.split(' ').slice(0,2).join(' '));
+      prevMissing = true;
+    }
+    
+    if (ccCourse) {
+      courses.push(
+        line.slice(line.search(/\|/) + 1) // line after |
+        .split(' ').slice(0,2).join(' ') // take first two words
+      );
+      prevMissing = false;
+    }
+    
+    if (uniCourse)
+      prevAnd = />\s*\&\s*<.*\|/.exec(line);
+  });
+  
+  return courses;
+}
+
+function getUnits(str) {
+  var units;
+  units = {};
+  str.split('\n').forEach((line) => {
+    // regex finds parens with nums, optional floating point number
+    var unitCnt = /\([0-9]+(\.[0-9]+)?\)$/.exec(line);
+    if (unitCnt) {
+      // the count is the string result, with cut off open and close parentheses
+      unitCnt = parseFloat(unitCnt[0].substr(1, unitCnt[0].length - 2));
+      var course = line.split('|')[1];
+      if (course) {
+        var andFound = course.search(/<b/i);
+        if (andFound !== -1)
+          course = course.substr(0, andFound);
+        else
+          course = course.substr(0, course.search(/\ \ /));
+        units[course] = unitCnt;
+      }
+    }
+  });
+  
+  return units;
+}
+
+function updateObject(base, updated) {
+  Object.keys(updated).forEach((key) => {
+    base[key] = updated[key];
+  });
+}
+
+function findCourseUnitsForPlan(cc, year, coursePlan) {
+  // takes a course plan object (required, choices, choosenum, missing)
+  var units = {};
+  
+  // add required courses
+  coursePlan.required.forEach((crs) => {
+    addCourseToUnits(units, cc, year, crs);
+  });
+  
+  // add the courses in the choices blocks
+  coursePlan.choices.forEach((choice) => {
+    console.log(choice);
+    choice.forEach((grp) => {
+      console.log(grp);
+      grp.forEach((crs) => {
+        addCourseToUnits(units, cc, year, crs);
+      });
+    });
+  });
+  
+  // add the courses in the chooseNum blocks
+  coursePlan.choosenum.forEach((grp) => {
+    grp.choices.forEach((crs) => {
+      addCourseToUnits(units, cc, year, crs);
+    });
+  });
+  
+  return units;
+}
+        
+function addCourseToUnits(units, cc, year, courseName) {
+  if (ccs[cc][year][courseName])
+    units[courseName] = ccs[cc][year][courseName];
+}
+  
+  
+
+module.exports = router;
